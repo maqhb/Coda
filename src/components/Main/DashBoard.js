5ceefc8c06e1ca6ee7bb276267a081580b31c6a8
Hamza-Alam <hamzaalam@linknbit.com>
2020-07-25 18:52:02 +0500

    Office Screen New Office Add Done

--- src/components/Main/DashBoard.js
+++ src/components/Main/DashBoard.js
@@ -5,7 +5,7 @@ import profile from '../../assets/dashboard/userProfile.PNG';
 import office from '../../assets/dashboard/office.jpg';
 import more from '../../assets/dashboard/more.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome,faHistory,faUsers,faCog,faEdit,faBuilding,faTimes,faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { faHome,faHistory,faUsers,faCog,faEdit,faBuilding,faTimes,faCamera } from "@fortawesome/free-solid-svg-icons";
 
 
 const DashBoard = () =>{
@@ -15,6 +15,9 @@ const DashBoard = () =>{
         {roomNo:"Room#5",user:"15 users",request:"12 support request",address:"Wall Street 123, California"},
         {roomNo:"Room#5",user:"15 users",request:"12 support request",address:"Wall Street 123, California"}
     ])
+    const dropdownShow = (e) =>{
+        
+    }
     const openMenu = () =>{
         if(document.getElementsByClassName('sideNav')[0].style.display=="block")
         {
@@ -29,6 +32,15 @@ const DashBoard = () =>{
             document.getElementsByClassName('sideNav')[0].style.width = "100%";
             document.getElementsByClassName("main-content")[0].style.display = "none";
             
+        }
+    }
+    const submitForm = () =>{
+        const officeName=document.getElementById('office-name').value;
+        const location=document.getElementById('location').value;
+        const request=document.getElementById('request').value;
+        if(officeName!="" && location!="" && request!=""){
+            const newObj={'roomNo':officeName,'user':'15 users','request':request,'address':location};
+            setNewOffice(state=>[...state,newObj]);
         }
     }
     return(
@@ -109,7 +121,7 @@ const DashBoard = () =>{
                                 <div className="room-details">
                                     <div className="container-fluid">
                                         <div className="row">
-                                            <div className="col-lg-3">
+                                            <div className="col-lg-3 col-md-6">
                                                 <div className="roomCard">
                                                     <div className="roomDetail y-l-b">
                                                         <h3>Room 3</h3>
@@ -120,7 +132,7 @@ const DashBoard = () =>{
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="col-lg-3">
+                                            <div className="col-lg-3 col-md-6">
                                                 <div className="roomCard">
                                                     <div className="roomDetail b-l-b">
                                                         <h3>Room 3</h3>
@@ -131,7 +143,7 @@ const DashBoard = () =>{
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="col-lg-3">
+                                            <div className="col-lg-3 col-md-6">
                                                 <div className="roomCard">
                                                     <div className="roomDetail g-l-b">
                                                         <h3>Room 3</h3>
@@ -142,7 +154,7 @@ const DashBoard = () =>{
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="col-lg-3">
+                                            <div className="col-lg-3 col-md-6">
                                                 <div className="roomCard">
                                                     <div className="roomDetail r-l-b">
                                                         <h3>Room 3</h3>
@@ -164,6 +176,60 @@ const DashBoard = () =>{
                                 </div>
                             </div>
                             <div id="offices" className="tab-pane fade">
+                            <div class="modal fade" id="myModal" role="dialog">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4 class="modal-title">New Room</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's. </p>
+                                            <div className="formDiv">
+                                                <div className="container-fluid">
+                                                    <div className="row">
+                                                        <div className="col-lg-6">
+                                                            <label>Name Offices</label>
+                                                            <input placeholder="Name Office" name="office-name" id="office-name"/>
+                                                        </div>
+                                                        <div className="col-lg-6">
+                                                            <label>Loation</label>
+                                                            <input id="location"/>
+                                                        </div>
+                                                    </div>
+                                                    <div className="row m-t-20">
+                                                        <div className="col-lg-12">
+                                                            <label>Button Request</label>
+                                                            <input id="request"/>
+                                                        </div>
+                                                    </div>
+                                                    <div className="row m-t-20">
+                                                        <div className="col-lg-5">
+                                                            <div className="uploadImage">
+                                                                <FontAwesomeIcon icon={faCamera}/>
+                                                                <p>Drop your logo here or</p>
+                                                               <label for="file-input"><span >Upload a Image</span>
+                                                                <input id="file-input" type="file" />
+                                                                </label> 
+  
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-lg-7">
+                                                            <div className="imgContentDiv">
+                                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-default cancelBtn" data-dismiss="modal">CANCEL</button>
+                                        <button type="button" class="btn btn-default createRoomBtn" data-dismiss="modal" onClick={submitForm}>CREATE ROOM</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                                 <div className="container-fluid">
                                     <div className="row">
                                         <div className="col-lg-3">
@@ -172,7 +238,7 @@ const DashBoard = () =>{
                                         <div className="col-lg-9">
                                             <div className="office-btn-div">
                                                 <span><a>DOWNLOAD ALL QR CODE</a></span>
-                                                <span><a>NEW OFFICE</a></span>
+                                                <span type="button"  data-toggle="modal" data-target="#myModal" className="modalBtn"><a>NEW OFFICE</a></span>
                                             </div>
                                         </div>
                                     </div>
@@ -180,12 +246,16 @@ const DashBoard = () =>{
                                 <div className="new-offices-div">
                                     <div className="container-fluid">
                                         <div className="row">
-                                        {newOffice.map((item)=>(
-                                            <div className="col-lg-3">
+                                        {newOffice.map((item,index)=>(
+                                            <div className="col-lg-3 col-md-3 col-sm-3">
                                                 <div className="offceDiv">
                                                     <div className="more-btn-img">
                                                         <img src={office}/>
-                                                        <img className="moreImg" src={more}/>
+                                                        <img className="moreImg" src={more} onClick={dropdownShow(index)}/>
+                                                            <div className="office-dropDown">
+                                                                <p><a>View QR Code</a></p>
+                                                                <p><a>Edit Room</a></p>
+                                                            </div>
                                                     </div>
                                                     <div className="conatentDiv">
                                                         <h4>{item.roomNo}</h4>
@@ -196,20 +266,6 @@ const DashBoard = () =>{
                                                 </div>
                                             </div>
                                         ))}
-                                            {/* <div className="col-lg-3">
-                                                <div className="offceDiv">
-                                                    <div className="more-btn-img">
-                                                        <img src={office}/>
-                                                        <img className="moreImg" src={more}/>
-                                                    </div>
-                                                    <div className="conatentDiv">
-                                                        <h4>Room#5</h4>
-                                                        <p>15 users</p>
-                                                        <p>12 support request</p>
-                                                        <p>Wall Street 123, California</p>
-                                                    </div>
-                                                </div>
-                                            </div> */}
                                         </div>
                                     </div>
                                 </div>
