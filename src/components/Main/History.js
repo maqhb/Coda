b23f06273222a43c56412d7ee658fd2e16fccbe4
AbdulWaheed <waheed.moeed123@gmail.com>
2020-08-06 17:17:11 +0500

    reverse and double reverse added

--- src/components/Main/History.js
+++ src/components/Main/History.js
@@ -12,35 +12,55 @@ class History extends Component{
             page: 0,
             itemNum:0
         }
+        this.fetchHistory = this.fetchHistory.bind(this)
         this.getHistory = this.getHistory.bind(this)
         this.itemNo=0
     }
 
     componentDidMount() {
         if(this.state.page === 0){
-            this.getHistory()
+            this.fetchHistory(1)
         }
     }
 
-
-    getHistory(){
-        Axios.post("https://kallpod-dev-php.ue.r.appspot.com/request/list?access_token="+Cookies.get("token")+"&page="+(this.state.page+1)).then((response)=>{
+    fetchHistory(page){
+        console.log(page)
+        Axios.post("https://kallpod-dev-php.ue.r.appspot.com/request/list?access_token="+Cookies.get("token")+"&page="+page).then((response)=>{
             if(response.data.success){
                 this.setState({
                     history : response.data.response.data,
-                    page:this.state.page+1,
+                    page:(this.state.page > 0)?page:1,
                     itemNum : response.data.response.from-1
                 })
             }
             else{
-                alert("Wrong Username or Password")
+                alert("Failed to get history")
             }
         }).catch((error)=>{
             alert(error)
         })
     }
-    render() {
 
+    getHistory(event){
+        event.preventDefault()
+        let page  = this.state.page
+        if(event.target.id === "doubleBack"){
+            console.log("double back clicked")
+            page = page - 2
+        } else if(event.target.id === "doubleNext"){
+            console.log("double next clicked")
+            page = page + 2
+        }else if(event.target.id === "next"){
+            page = page +1 //single back
+            console.log("next clicked")
+        }else{
+            page = page - 1
+            console.log("back clicked")
+        }
+        this.fetchHistory(page)
+    }
+
+    render() {
         if(this.state.history === null){
             return (
                <>
@@ -49,7 +69,6 @@ class History extends Component{
         }
         else{
             this.itemNo = this.state.itemNum
-            console.log(this.itemNo)
               return(
                 <div id="history" className="tab-pane fade">
                 <h3>Historical Support Request</h3>
@@ -103,24 +122,12 @@ class History extends Component{
                                             <div className="row">
                                                 <div className="col-lg-12">
                                                     <div className="multipleDiv">
-                                                            <div className="item-per-page">
-                                                                <label>Item per page:</label>
-                                                                <select>
-                                                                    <option>1</option>
-                                                                    <option>2</option>
-                                                                    <option>3</option>
-                                                                    <option>4</option>
-                                                                    <option>5</option>
-                                                                </select>
-                                                            </div>
-                                                            <div className="paginationText">
-                                                                <p>1-5 of 20</p>
-                                                            </div>
                                                             <div className="arrowsDiv">
-                                                                <i class="fa fa-angle-double-left" aria-hidden="true"></i>
-                                                                <i class="fa fa-angle-left" aria-hidden="true"></i>
-                                                                <i class="fa fa-angle-right" aria-hidden="true" onClick={this.getHistory}></i>
-                                                                <i class="fa fa-angle-double-right" aria-hidden="true"></i>
+                                                                <i class="fa fa-angle-double-left" id="doubleBack" style={{color:(this.state.page > 2)?"#000":""}} aria-hidden="true" onClick={this.getHistory}></i>
+                                                                <i class="fa fa-angle-left" style={{color:(this.state.page === 1)?"":"#000"}} aria-hidden="true" onClick={this.getHistory}></i>
+                                                                <i style={{fontSize: "20px", margin:"5px", paddingLeft:"12px"}}>{this.state.page}</i>
+                                                                <i class="fa fa-angle-right" id="next" aria-hidden="true" onClick={this.getHistory}></i>
+                                                                <i class="fa fa-angle-double-right" id="doubleNext"  aria-hidden="true" onClick={this.getHistory}></i>
                                                             </div>
                                                         </div>
                                             </div>
