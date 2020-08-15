8f4882333dc98be6924b6586f1bc8829525d1f60
maqhb <31518888+maqhb@users.noreply.github.com>
2020-08-16 03:47:21 +0500

    Settings

--- src/components/Main/DashBoard.js
+++ src/components/Main/DashBoard.js
@@ -12,6 +12,7 @@ import MainDashboard from "./MainDashboard";
 import Actions from "./Actions";
 import Users from "./Users";
 import Logout from "../auth/Logout";
+import Settings from "../Main/Settings";
 
 const DashBoard = (props) =>{
     const [isShow,setIsShow]=useState({
@@ -71,10 +72,8 @@ const DashBoard = (props) =>{
         }
         console.log(newButton.index);
     }
-    const [password,setPassword]=useState(false);
-    const togglePasswordVisiblity = () => {
-        setPassword(password ? false : true);
-    };
+    
+    
 
     return(
         <section id="dashBoard">
@@ -208,99 +207,8 @@ const DashBoard = (props) =>{
                                 </div>
                             </div>
                             <Users/>
-                            <div id="setting" className="tab-pane fade">
-                                <h3>About you</h3>
-                                <div className="profile-image-div">
-                                    <div className="container-fluid">
-                                        <div className="row">
-                                            <div className="col-lg-2 col-md-2 col-sm-2">
-                                                <img className="img-responsive" src={avatar}/>
-                                            </div>
-                                            <div className="col-lg-10 col-md-10 col-sm-10">
-                                                <div className="image-content">
-                                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                                                </div>
-                                                <label for="file-input" className="chnageAvatar"><span >CHANGE AVATAR</span>
-                                                    <input id="file-input" type="file" />
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="edit-profile-div">
-                                    <div className="container-fluid">
-                                        <div className="row">
-                                            <div className="col-lg-6 col-md-6 col-sm-6">
-                                                
-                                                <label>Name</label>
-                                                <input placeholder="First Name" className="m-b-10"/>
-                                            </div>
-                                            <div className="col-lg-6 col-md-6 col-sm-6">
-                                                <label>Last Name</label>
-                                                <input placeholder="Last Name"/>
-                                            </div>
-                                        </div>
-                                        <div className="row m-t-20">
-                                            <div className="col-lg-6 col-md-6 col-sm-6">
-                                                <label>Email</label>
-                                                <input placeholder="comapny@example.com" className="m-b-10"/>
-                                            </div>
-                                            <div className="col-lg-6 col-md-6 col-sm-6 ">
-                                            <div className="inputPassword">
-                                                <label>Password</label>
-                                                <input placeholder="Password"  type={password ? "text" : "password"}></input>
-                                                <FontAwesomeIcon onClick={togglePasswordVisiblity} icon={faEye}/>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="about-company">
-                                    <h3>About your Company</h3>
-                                    <div className="row">
-                                        <div className="col-lg-12">
-                                            <label>Company Name</label>
-                                            <input placeholder="Company Name"/>
-                                        </div>
-                                    </div>
-                                    <div className="row m-t-20">
-                                        <div className="col-lg-3 col-md-3 col-sm-3">
-                                            <div className="uploadImage">
-                                                <FontAwesomeIcon icon={faCamera}/>
-                                                <p>Drop your logo here or</p>
-                                                <label for="file-input"><span >Upload a Image</span>
-                                                    <input id="file-input" type="file" />
-                                                </label> 
-                                            </div>
-                                        </div>
-                                        <div className="col-lg-9 col-md-9 col-sm-9">
-                                        <div className="image-content">
-                                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                                                </div>
-                                                <label for="file-input" className="chnageAvatar"><span >CHANGE LOGO</span>
-                                                    <input id="file-input" type="file" />
-                                                </label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr />
-                                <div className="lastSection">
-                                    <div className="container-fluid">
-                                        <div className="row">
-                                            <div className="col-lg-12">
-                                                <div className="btnDiv">
-                                                    <span>
-                                                        <a href="">CANCELAR</a>
-                                                    </span>
-                                                    <span>
-                                                        <a href="">GUARDAR CAMBIOS</a>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            {/* Settings */}
+                            <Settings/>
                             <div id="button-services" className="tab-pane fade">
                             <div class="modal fade" id="buttonModal" role="dialog">
                                 <div class="modal-dialog">
